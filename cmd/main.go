7d2ee8201bfe706c71ee4f0032326d3371023d71
Nick Dumas <dumasn@homes.com>
2022-02-18 10:34:51 -0500

    First working draft of the mob spawner. Super crude.

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os/signal"
+	"syscall"
+	"time"
 	// "fmt"
 	"os"
 
@@ -13,9 +16,13 @@ import (
 func main() {
 	w := &ecs.World{}
 
-	var renderable *systems.Renderable
-	trs := &systems.TerminalRenderSystem{W: os.Stdout, Entities: make(map[uint64]systems.Renderable)}
-	w.AddSystemInterface(trs, renderable, nil)
+	// var renderable *systems.Renderable
+	// trs := &systems.TerminalRenderSystem{W: os.Stdout, Entities: make(map[uint64]systems.Renderable)}
+	// w.AddSystemInterface(trs, renderable, nil)
+
+	var spawnable *systems.Spawnable
+	mss := &systems.MobSpawnerSystem{Entities: make(map[uint64]systems.Spawnable)}
+	w.AddSystemInterface(mss, spawnable, nil)
 
 	p := components.Player{BasicEntity: ecs.NewBasic()}
 	p.RenderComponent.Type = components.RenderPlayer
@@ -30,6 +37,25 @@ func main() {
 	m.RenderComponent.Type = components.RenderMob
 
 	w.AddEntity(&p)
-	w.AddEntity(&m)
-	w.Update(1)
+	// w.AddEntity(&m)
+	t := time.NewTicker(1 * time.Second)
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case <-t.C:
+				w.Update(1)
+
+			}
+		}
+	}()
+
+	// Wait here until CTRL-C or other term signal is received.
+	// fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+	done <- true
 }
--- systems/spawner.go
+++ systems/spawner.go
@@ -11,7 +11,9 @@ type Spawnable interface {
 }
 
 type MobSpawnerSystem struct {
-	World *ecs.World
+	World    *ecs.World
+	Entities map[uint64]Spawnable
+	Clock    float64
 }
 
 func (mss *MobSpawnerSystem) New(w *ecs.World) {
@@ -19,14 +21,34 @@ func (mss *MobSpawnerSystem) New(w *ecs.World) {
 }
 
 func (mss *MobSpawnerSystem) AddByInterface(o ecs.Identifier) {
-	obj := o.(Renderable)
+	obj := o.(Spawnable)
 	mss.Add(obj)
 }
 
-func (mss *MobSpawnerSystem) Add(r Renderable) {
-	// mss.Entities[r.GetBasicEntity().ID()] = r
+func (mss *MobSpawnerSystem) Add(s Spawnable) {
+	mss.Entities[s.GetBasicEntity().ID()] = s
 	// mss.Entities = append(mss.Entities, r)
 }
 
-func (mss MobSpawnerSystem) Update(dt float32)        {}
+func (mss MobSpawnerSystem) Update(dt float32) {
+	// here's where i check how many enemies there are, how long we've been playing, the difficult level, etc and then spawn more
+	// i want to use the update interval as part of determining spawn rate but i'm not sure how to build a formula for it
+	m := NewMob(1, 0)
+	mss.World.AddEntity(&m)
+	mss.Clock += float64(dt)
+
+}
+
 func (mss MobSpawnerSystem) Remove(e ecs.BasicEntity) {}
+
+// NewMob generates a single new Mob unit.
+//   level: the starting level of the generated mob. this determines starting HP, Mana, how many and which modifiers could be applied, etc
+//   mod: 0 disables modifiers; higher values make modifiers more likely, more plentiful, and stronger
+func NewMob(level int, mod int) components.Mob {
+	m := components.Mob{BasicEntity: ecs.NewBasic()}
+	m.RenderComponent.Type = components.RenderMob
+	m.Health.Max = level * 30
+	m.Health.Total = m.Health.Max
+
+	return m
+}
